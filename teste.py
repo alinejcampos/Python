3e3bd6236b36a8e3c6366b1b7e41483b6058cccc
alinejcampos <89116448+alinejcampos@users.noreply.github.com>
2023-06-15 19:31:43 -0300

    Teste

---
+++ teste.py
@@ -0,0 +1,12 @@
+is_ready = 15.5
+print(type(is_ready))
+
+is_ready = True
+fuel_deposit = 59.89
+best_grade = "A"
+number_of_pets = 2
+print(type(is_ready))
+print(type(fuel_deposit))
+print(type(best_grade))
+print(type(number_of_pets))
+
